be2120993d0e64fbd8fcf02c92d9f8ad5ed3ae46
Federico Bonofiglio <federico.bonofiglio@veramed.co.uk>
2022-04-25 20:23:51 +0100

    moving functions to utils and checking for errors. Passed

--- app.R
+++ app.R
@@ -1,113 +1,23 @@
-# For extra info on HiSCR please see https://pubmed.ncbi.nlm.nih.gov/26201313/
-# 
 
-library(tidyverse)
-library(shiny)
-library(epitools)
+# UI
 
-data <- read.csv("https://raw.githubusercontent.com/VIS-SIG/Wonderful-Wednesdays/master/data/2022/2022-04-13/HiSCR_dat.csv")
+ui <- fluidPage(
+  
+  
+  
+)
 
-data %>% head()
-data %>% str()
 
-# Endpoint function: RR at week 16
-RR <- function(.data, TRT, HiSCR, 
-               sm = c("RR", "OR"), 
-               method_RR = c("wald", "small", "boot"),
-               method_OR = c("midp", "fisher", "wald", "small"))
+# Server logic
+
+server <- function(input, output)
 {
-  sm <- match.arg(sm)
-  method_RR <- match.arg(method_RR)
-  method_OR <- match.arg(method_OR)
-  tab <- .data %>% 
-    group_by({{TRT}}, {{HiSCR}}) %>% 
-    summarise(n = n()) %>% 
-    arrange(desc({{TRT}})) %>% 
-    pull(n) %>% 
-    matrix(nrow = 2) %>% 
-    t()
   
-  dimnames(tab) <- list("Treatment" = rev(unique(.data %>% select({{TRT}}) %>% pull() )),
-                        "Outcome" = unique(.data %>% select({{HiSCR}}) %>% pull()))
-  
-  switch(sm,
-         RR = riskratio(tab, method = method_RR),
-         OR = oddsratio(tab, method = method_OR)
-         )
   
   
 }
 
-#RR(data, TRT, HiSCR, sm = "OR")
-
-#' Computes AN count
-#' @param .data - data.frame as given by Github repo above
-#' @param abscesses - character - label for variable number of abscesses
-#' @param infl_nodes - character - label for variable number of inflammatory nodules
-AN_count <- function(.data, abscesses, infl_nodes)
-{
-  .data$abscesses + .data$infl_nodes
-}
-#
-#' Measure fraction change from baseline
-#' @param .data - data.frame as given by Github repo above
-#' @param baseline - numeric vector
-#' @param week16 - numeric vector 
-change <- function(.data, baseline, week16, method = c("fraction", "absolute"))
-{
-  method <- match.arg(method)
-  
-  switch(method,
-         fraction = {
-           if (any(.data$baseline) <= 0)
-             stop("All baseline values must be positive")
-           
-           with(.data,
-                (week16 - baseline)/baseline
-           )
-           
-         },
-         absolute = {
-           with(.data,
-                week16 - baseline
-           )
-           
-         }
-         )
-  
-
-}
-
-#' Recalculate HiSCR based on new definitions
-#' @param data - data.frame as given by Github repo above
-#' @param AN_incr - scalar numeric - Negative value for fraction change in AN counts relative to baseline
-#' @param fist_incr - scalar numeric - Negative value for absolute change in number of draining fistula relative to baseline
-#' @param abscesses_incr - scalar numeric - Negative value for absolute change in number of abscesses relative to baseline
-#' @param checkupvs - character - label of original HiSCR variable to check up new results to
-HiSCR <- function(.data, AN_incr = -0.5, fist_incr = 0, abscesses_incr = 0, checkupvs = "HiSCR")
-{
-  if (AN_incr > 0)
-    stop("AN_increase (AN count increase) must be negative")
-  if (fist_incr > 0 | abscesses_incr > 0)
-    stop("fist_incr and abscesses_incr (increase in number of draining fistulae or abscesses) must be negative")
-  
-  .data$AN_count.base <- .data %>% AN_count("abscesses.base", "infl.nod.base")
-  .data$AN_count.w16 <- .data %>% AN_count("abscesses.w16", "infl.nod.w16")
-  .data$AN_change <- .data %>% change("AN_count.base", "AN_count.w16", method = "fraction")
-  .data$fist_change <- .data %>% change("drain.fist.base", "drain.fist.w16", method = "absolute")
-  .data$abscess_change <- .data %>% change("abscesses.base", "abscesses.w16", method = "absolute")
-  
-  .data %>% mutate(newHiSCR = if_else(
-    AN_change <= AN_incr & fist_change <= fist_incr & abscess_change <= abscesses_incr,
-    "yes", "no"
-  )  )
-  
-  
-  # returns dataset with new HiSCR
-}
 
 
 
-# TODO shiny app capture printout --- create time animation for varying HiSCR definitions
-
-# Change in treatment effect at W 16 (RR or OR) for varying HiSCR definitions (animatet plot), Y-axis = sm, X-axis = AN_increase 
+shinyApp(ui, server)
